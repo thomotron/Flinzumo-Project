4cc094c4c36ec66ff725b8a276c7b04014e045df
Thomas Wade <gigatech55@gmail.com>
2019-01-31 14:48:14 +1030

    Use debuggable pause define

--- Project/Project.ino
+++ Project/Project.ino
@@ -79,19 +79,21 @@ void partI()
 void partII()
 {
   drive(1, RELSPEED_NORMAL, 1, false);  // Overshoot line at embassy a little
-  button.waitForButton();
-  spin(90);               // Turn to face south
-  button.waitForButton();
+  PAUSE
+  spin(90);                             // Turn to face south
+  PAUSE
   drive(5, RELSPEED_NORMAL, 1, false);  // Drive a bit to get off the embassy alignment line
-  button.waitForButton();
+  PAUSE
   drive(20, RELSPEED_NORMAL, 1, true);  // Drive into the blue sticks until the alignment line
-  button.waitForButton();
+  PAUSE
   drive(3, RELSPEED_NORMAL, 1, false);  // Push the sticks into the safe zone
-  button.waitForButton();
+  PAUSE
   drive(3, -RELSPEED_NORMAL, 1, true);  // Reverse until the alignment line
-  button.waitForButton();
+  PAUSE
+  drive(1, -RELSPEED_NORMAL, 1, false);  // Drive a bit to get off the alignment line
+  PAUSE
   drive(20, -RELSPEED_NORMAL, 1, true); // Reverse until the embassy
-  button.waitForButton();
-  spin(-90);              // Turn to face east
+  PAUSE
+  spin(-90);                            // Turn to face east
 }
 
